8e144594b73578bcc2f72a25139a21adb74232ca
Mohamed Essamaali <56739312+Mohamed-Essamaali@users.noreply.github.com>
2021-10-24 21:19:43 -0400

    Update homePage.js

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState} from "react"
 import '../App.css';
 import { Card,CardImg } from "reactstrap";
 import img from "../newBorn.jpg"
@@ -6,11 +6,27 @@ import { useHistory } from "react-router-dom";
 
 
 const HomePage = ()=>{
-    const {push} = useHistory()
+    const {push} = useHistory();
+   
+    const[name,setName] = useState("");
+    
+    const luckyName(){
+      var x = Math.random()*names.length;
+        setName(names[Math.ceil(x)-1]);
+    }
+     
+
+    console.log(names[Math.ceil(x)-1]);
 
 
     return (
         <div className='home'>
+            
+            <h1>Choose your Lucky Name</h1>
+            <h4> {name}</h4>
+        
+            <button onClick={luckyName}> My Luck </button>
+            
             <h1>Find out what you need</h1>
             <CardImg src={img} alt="can't find it"/>
             <button className='start-button' onClick={()=>{push('/links')}}>More Info</button>
